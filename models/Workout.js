8be4f81b4b0de1aae9de8ef12aef264b8b185e24
Giovanni Sosa <gio53196@gmail.com>
2021-10-14 20:57:02 -0700

    Added data to Workout model file

--- models/Workout.js
+++ models/Workout.js
@@ -2,9 +2,42 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+//Model with seed data
 const WorkSchema = new Schema({
-  author: String,
-  title: String
+  day: {
+      type: Date,
+      default: Date.now
+  },
+  exercises: [
+      {
+       type: {
+           type: String,
+           trim: true,
+           required: 'Select exercise type!',
+       },
+       name: {
+           type: String,
+           trim: true,
+           required: 'Set exercise name!',
+       },
+       duration: {
+           type: Number,
+           required: 'Enter the length of the workout in minutes.'
+       },
+       distance: {
+           type: Number,
+       },
+       weight: {
+           type: Number,
+       },
+       sets: {
+           type: Number,
+       },
+       reps: {
+           type: Number,
+       }
+      }
+  ]
 });
 
 const Work = mongoose.model("Workout", WorkSchema);
